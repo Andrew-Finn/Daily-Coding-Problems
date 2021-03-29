c72ed945ed37b2e3a7a7112a2e6410cb9b66d4d8
Andrew Finn <andrew.finn8@mail.dcu.ie>
2021-03-29 22:59:00 +0100

    2021.03.29

--- 2021/03 March/10th.py
+++ 2021/03 March/10th.py
@@ -2,7 +2,8 @@
 
 # This problem was asked by Netflix.
 
-# Given a sorted list of integers of length N, determine if an element x is in the list without performing any multiplication, division, or bit-shift operations.
+# Given a sorted list of integers of length N, determine if an element x is in the list without performing any
+# multiplication, division, or bit-shift operations.
 
 # Do this in O(log N) time.
 
@@ -16,18 +17,19 @@ def binary_search(l, s):
         return binary_search((l[len(l) // 2:]), s)
     return binary_search((l[:len(l) // 2]), s)
 
+
 if __name__ == "__main__":
     import random
+
     for i in range(500):
-        l = sorted([random.randint(1,999) for x in range(99)])
+        l = sorted([random.randint(1, 999) for x in range(99)])
         for i in range(10):
             s = random.choice(l)
             assert binary_search(l, s) == True
 
         for _ in range(10):
             while True:
-                n = random.randint(1,999)
+                n = random.randint(1, 999)
                 if n not in l:
                     assert binary_search(l, n) == False
                     break
-                    
